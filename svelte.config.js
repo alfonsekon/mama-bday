78be4a05740c02f659fa18dac21c10e7ac3dc05a
alfonsekon <alfonso.luis.delosreyes@gmail.com>
2025-04-18 12:06:41 +0800

    build

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
-
-const base = process.env.VITE_BASE_PATH || '';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
 	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'index.html'
+			fallback: '404.html'
 		}),
 		paths: {
-			base
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
