e160fbeafc46708e760384235e788471170cbfe5
alfonsekon <alfonso.luis.delosreyes@gmail.com>
2025-04-18 12:08:06 +0800

    build

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 export const prerender = true;
 <script lang="ts">
     import '../app.css';
-    import { base } from '$app/paths';
 </script>
 
 <nav style="justify-content: center;">
     <a href="/photos">Photos</a>
-    <a href="{base}/">Home</a>
+    <a href="/">Home</a>
     <a href="/letters">Letters</a>
 </nav>
 
